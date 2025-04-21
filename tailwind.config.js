a5d66da96f5a98d5ceac53b0fe45aaae26e70e9a
Tim Moore <timjimmoore@gmail.com>
2025-04-21 11:55:09 +0100

    Poppins font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ module.exports = {
                 primary: themeStyle.primary
             },
             fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-                serif: ['Roboto Slab', 'serif']
+                sans: ['Poppins', 'sans-serif'],
+                serif: ['Poppins', 'serif']
             },
             gridTemplateColumns: {
                 16: 'repeat(16, minmax(0, 1fr))'
